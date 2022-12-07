1b3150923ef44316b5ad26a2989e0e0c192e9153
nutcras <83870987+nutcras@users.noreply.github.com>
2022-12-07 15:23:34 +0700

    update

--- oop/poly/first/MoveTo.java
+++
@@ -1,8 +0,0 @@
-package oop.poly.first;
-
-public class MoveTo {
-
-  public void Move() {
-    System.out.println("ขยับจากที่เดิม");
-  }
-}
--- oop/poly/first/Test.java
+++
@@ -1,7 +0,0 @@
-package oop.poly.first;
-
-public class Test {
-  public static void main(String[] args) {
-
-  }
-}
