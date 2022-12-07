c6c621ea9a4fb0f0fa84a4c62a265db94ad25a5a
nutcras <83870987+nutcras@users.noreply.github.com>
2022-12-07 14:36:49 +0700

    update Sd

---
+++ oop/Polymorphism/first/Main.java
@@ -0,0 +1,11 @@
+package oop.Polymorphism.first;
+
+public class Main {
+  public static void main(String[] args) {
+    Cars Toyota = new Cars();
+    // ติดอยู่ตรงนี้
+    Toyota.Move();
+    Toyota.start();
+
+  }
+}
